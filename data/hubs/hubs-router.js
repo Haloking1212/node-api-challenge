a26aa30ffd0271bafba1d4182d23fbe00353490e
Hector Valdez <hvaldez180@gmail.com>
2020-01-24 22:17:03 -0500

    added post reqest for actions

--- data/hubs/hubs-router.js
+++ data/hubs/hubs-router.js
@@ -15,4 +15,22 @@ router.get('/api/actions', (req, res) => {
     })
 })
 
+//Post request for Actions
+
+router.post('/api/actions', (req, res) => {
+    const { description, notes } = req.body
+    const project_id = req.params.id
+    if(!description || !notes || project_id){
+        return res.status(404).json({ message: "Please provide a description, notes and previous project id" })
+    }
+    Hubs.insert(req.body)
+    .then(hubs => {
+        res.status(201).json(hubs);
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({ message: "Error posting actions" })
+    })
+})
+
 module.exports = router;
